ef9d8fd46af39c1045a0aad2a992572fb093b036
Pedro Elizalde <pedro.elizalde@ing.austral.edu.ar>
2022-06-15 10:53:17 -0300

    minor changes

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import {Request, Response} from 'express';
 import {PrismaClient} from '@prisma/client'
 
-require("dotenv").config()
+
 const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
@@ -10,34 +10,29 @@ const SpotifyWebApi = require("spotify-web-api-node")
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.json());
 
 const prisma = new PrismaClient();
 
-app.post("/", async (req: Request, res: Response) => {
-    const {accessToken, refreshToken, expireTime} = req.body;
-    const user = await prisma.user.create({
-        data: {
-            accessToken: accessToken,
-            refreshToken: refreshToken,
-            expireTime: Number(expireTime),
-        },
-    });
-    res.json(user)
-});
-
-app.get("/", async (req: Request, res: Response) => {
+//get all users
+app.get("/getUsers", async (req: Request, res: Response) => {
     const users = await prisma.user.findMany();
     res.json(users);
 });
 
+//get all queues
 app.get("/getQueues", async (req: Request, res: Response) => {
-    const users = await prisma.queue.findMany();
-    res.json(users);
+    const queues = await prisma.queue.findMany();
+    res.json(queues);
 });
 
-app.get("/byId/:id", async (req: Request, res: Response) => {
+//get all songs
+app.get("/getSongs", async (req: Request, res: Response) => {
+    const songs = await prisma.song.findMany();
+    res.json(songs);
+});
+
+//get user by ID
+app.get("/userId/:id", async (req: Request, res: Response) => {
     res.setHeader("Access-Control-Allow-Origin","http://localhost:3000")
     const id = req.params.id;
     const user = await prisma.user.findUnique({
@@ -48,46 +43,86 @@ app.get("/byId/:id", async (req: Request, res: Response) => {
     res.json(user);
 });
 
-app.put("/", async (req: Request, res: Response) => {
-    const {id, accessToken} = req.body
-    const updateUser = await prisma.user.update({ 
-        where: {
-            id: id,
-        },
-        data: {
-            accessToken: accessToken,
+//get queue by ID
+app.get("/queueId/:id", async (req: Request, res: Response) => {
+    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000")
+    const id = req.params.id;
+    const queue = await prisma.queue.findUnique({
+        where:{
+            id: id
         }
-    })
-    res.json(updateUser);
+    });
+    res.json(queue);
 });
 
-app.delete("/:id", async(req: Request, res: Response) => {
-    const id = req.params.id;
-    const deletedUser = await prisma.user.delete({
+//get songs by queueId
+app.get("/:queueId/songs", async (req: Request, res: Response) => {
+    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000")
+    const queueId = req.params.queueId;
+    const songs = await prisma.song.findMany({
         where:{
-            id: id,
+            queueId: queueId,
+        }
+    });
+    res.json(songs);
+});
+
+//get songs by uri
+app.get("/songs/:uri", async (req: Request, res: Response) => {
+    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000")
+    const uri = req.params.uri;
+    const song = await prisma.song.findMany({
+        where:{
+            uri: uri,
+        }
+    });
+    res.json(song);
+});
+
+//like song
+app.put("/:queueId/like", async (req: Request, res: Response) => {
+    const {uri, queueId} = req.body;
+    const likedSong = await prisma.song.updateMany({
+        where:{
+            uri: uri,
+            queueId: queueId,
+        },
+        data:{ 
+            likes: +1 //not working - Pedro
+        }
+    })
+    res.json(likedSong);
+});
+
+//dislike song
+app.put("/:queueId/dislike", async (req: Request, res: Response) => {
+    const {uri, queueId} = req.body;
+    const dislikedSong = await prisma.song.updateMany({
+        where:{
+            uri: uri,
+            queueId: queueId,
+        },
+        data:{ 
+            dislikes: +1 //not working - Pedro
+        }
+    })
+    res.json(dislikedSong);
+});
+
+//delete song
+app.delete("/:queueId/deleteSong", async(req: Request, res: Response) => {
+    const queueId = req.params.queueId;
+    const uri = req.body.uri;
+    const deletedUser = await prisma.song.deleteMany({
+        where:{
+            uri: uri,
+            queueId:queueId,
         }}
     );
     res.json(deletedUser);
 });
 
-//app.post("/", async (req: Request, res: Response) => {
-//    const {accessToken, refreshToken, expireTime} = req.body;
-//    const user = await prisma.user.create({
-//        data: {
-//            accessToken: accessToken,
-//            refreshToken: refreshToken,
-//            expireTime: Number(expireTime),
-//            Queue: {
-//                create: {
-//                    userId: '',
-//                },
-//            },
-//        },
-//    });
-//    res.json(user)
-//});
-
+//add song
 app.post("/addSong", async (req: Request, res: Response) => {
     const {uri, queueId, likes, dislikes} = req.body;
     const song = await prisma.song.create({
@@ -101,8 +136,9 @@ app.post("/addSong", async (req: Request, res: Response) => {
     res.json(song)
 });
 
+//login with spotify
 app.post("/login", async (req: Request, res: Response) => {
-    res.setHeader("Access-Control-Allow-Origin","*")    
+      
     const code = req.body.code
     const spotifyApi = new SpotifyWebApi({
       redirectUri: process.env.REDIRECT_URI,
@@ -117,10 +153,10 @@ app.post("/login", async (req: Request, res: Response) => {
             data:{
                 accessToken: data.body.access_token,
                 refreshToken: data.body.refresh_token,
-                
+                //fix expireIn - Pedro
             },
         });
-        const queue = await prisma.queue.create({
+        await prisma.queue.create({
             data:{ 
               userId: user.id,  
             },
@@ -132,8 +168,10 @@ app.post("/login", async (req: Request, res: Response) => {
       })
   })
 
-  app.post("/refresh", (req: Request, res:Response) => {
-    res.setHeader("Access-Control-Allow-Origin","*")    
+  //refresh spotify access_token & expiretime
+  app.post("/refresh/:id", async (req: Request, res:Response) => {
+    res.setHeader("Access-Control-Allow-Origin","*")
+    const id = req.params.id;
     const refreshToken = req.body.refreshToken
     const spotifyApi = new SpotifyWebApi({
       redirectUri: process.env.REDIRECT_URI,
@@ -144,10 +182,18 @@ app.post("/login", async (req: Request, res: Response) => {
   
     spotifyApi
       .refreshAccessToken()
-      .then((data: { body: { accessToken: String; expiresIn: any; }; }) => {
+      .then( async(data: { body: { access_token: string; }; }) => {
+        const user = await prisma.user.update({ 
+            where:{
+                id:id
+            },
+            data:{
+                accessToken: data.body.access_token,
+                
+            }
+        });
         res.json({
-          accessToken: data.body.accessToken,
-          expiresIn: data.body.expiresIn,
+          accessToken: user.accessToken,
         })
       })
       .catch((err: String) => {
@@ -155,6 +201,21 @@ app.post("/login", async (req: Request, res: Response) => {
         res.sendStatus(400)
       })
   })
+
+  //guest login (anon)
+  app.post("/guest", async (req: Request, res: Response)=>{
+    res.setHeader("Access-Control-Allow-Origin","*") 
+    const guest = await prisma.user.create({
+        data:{ 
+            accessToken:null,
+            refreshToken:null,
+        }
+    })
+    res.json(guest)
+  })
+
+
+
 
 app.listen(3001,() => {
     console.log('SERVER RUNNING ON PORT 3001')
